da436822544a48ed6bbbf78999a1956db531e9e4
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2021-11-24 22:55:27 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -16,12 +16,14 @@ import copy
 
 
 def Q1a():
-    pass
+
+  return Configuration({'screenPosition' : -5, 'xAxisColor' : [1,1,0]}).display()
+    
     
 def Q1b_f():
     return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}). \
         setParameter('xAxisColor', [1, 1, 0]). \
-        setParameter('yAxisCo lor', [0,1,1]). \
+        setParameter('yAxisColor', [0,1,1]). \
         display()
         
 def Q2b():
@@ -94,10 +96,10 @@ def Q6():
 def main():
     # Enlever un des commentaires pour la question traitée
     
-    configuration = Q1a()
-    # configuration = Q1b_f()
-    # configuration = Q2b()
-    # configuration = Q2c()
+    #configuration = Q1a()
+    #configuration = Q1b_f()
+    #configuration = Q2b()
+    configuration = Q2c()
     # configuration = Q3a()
     # configuration = Q4a()
     # configuration = Q5a()
@@ -111,3 +113,4 @@ def main():
 # Calls the main function
 if __name__ == "__main__":
     main()    
+    
