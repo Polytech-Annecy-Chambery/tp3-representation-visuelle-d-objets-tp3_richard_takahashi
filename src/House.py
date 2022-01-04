175b60d0fea993af7a3b0fbe39e1a58e74b8d9a0
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-01-04 21:56:15 +0100

    Update House.py

--- src/House.py
+++ src/House.py
@@ -42,4 +42,10 @@ class House:
     # Draws the house      
     def draw(self):  
         # A compléter en remplaçant pass par votre code
-        pass        
+        gl.glPushMatrix()
+        gl.glRotatef(self.parameters['orientation'], 0, 0, 1)
+        gl.glTranslatef(self.parameters['position'][0], self.parameters['position'][1], self.parameters['position'][2])
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL)
+        for i in self.objects :
+          i.draw()
+        gl.glPopMatrix()        
