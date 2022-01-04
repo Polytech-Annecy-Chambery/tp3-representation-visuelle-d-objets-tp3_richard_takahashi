872e5e6e6580f2e5382f6fa1cded622e8cbde6cd
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-01-04 21:57:59 +0100

    Update Section.py

--- src/Section.py
+++ src/Section.py
@@ -78,29 +78,52 @@ class Section:
     # Checks if the opening can be created for the object x
     def canCreateOpening(self, x):
         # A compléter en remplaçant pass par votre code
-        pass      
+        if x.parameters['thickness'] == self.parameters['thickness'] :
+          if x.parameters['position'][0] >= self.parameters['position'][0] :
+            if x.parameters['position'][1] >= self.parameters['position'][1] :
+              if x.parameters['position'][2] >= self.parameters['position'][2] :
+                if x.parameters['position'][2] + x.parameters['height'] <= self.parameters['position'][2] + self.parameters['height']:
+                  if x.parameters['position'][0] + x.parameters['width'] <= self.parameters['position'][0] + self.parameters['width']:
+                    return True 
+        return False
+
         
+
     # Creates the new sections for the object x
-    def createNewSections(self, x):
+    def createOpening(self, x):
         # A compléter en remplaçant pass par votre code
-        pass              
+        if self.canCreateOpening(x) :
+          L = []
+          s1 = Section({'position': self.parameters['position'], 'width': x.parameters['position'][0] - self.parameters['position'][0], 'height': self.parameters['height'], 'thickness': self.parameters['thickness'], 'color' :[1, 0, 0]})
+          L.append(s1)
+
+          s2 = Section({'position': [x.parameters['position'][0] + x.parameters['width'], self.parameters['position'][1], self.parameters['position'][2]], 'width': - self.parameters['position'][0] + self.parameters['width'] - x.parameters['position'][0] - x.parameters['width'], 'height': self.parameters['height'], 'thickness': self.parameters['thickness'], 'color' :[0, 1, 0]})
+          L.append(s2)
+
+          s3 = Section({'position': [x.parameters['position'][0], self.parameters['position'][1], self.parameters['position'][2]], 'width': x.parameters['width'], 'height': x.parameters['position'][2] - self.parameters['position'][2], 'thickness': self.parameters['thickness'], 'color' :[0, 0, 1]})
+          L.append(s3)
+
+          s4 = Section({'position': [x.parameters['position'][0], x.parameters['position'][1], x.parameters['position'][2] + x.parameters['height']], 'width': x.parameters['width'], 'height': self.parameters['height'] - x.parameters['height'] - x.parameters['position'][2], 'thickness': self.parameters['thickness']})
+          L.append(s4)
+
+          return L
         
     # Draws the edges
     def drawEdges(self):
         # A compléter en remplaçant pass par votre code
+        gl.glPushMatrix()
+        gl.glTranslatef(self.parameters['position'][0], self.parameters['position'][1], self.parameters['position'][2])
+        gl.glBegin(gl.GL_LINES) # Indique que l'on va commencer un trace en mode lignes (segments)
+        gl.glColor3fv([0, 0, 0]) # Indique la couleur du prochian segment en RGB
         
-        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
-        gl.glBegin(gl.GL_QUADS)
-        gl.glColor3fv([0.2,0.2,0.2])
+        gl.glVertex3fv(self.vertices[0]) # Premier vertice : départ de la ligne
+        gl.glVertex3fv(self.vertices[3]) # Deuxième vertice : fin de la ligne
 
         gl.glVertex3fv(self.vertices[0])
         gl.glVertex3fv(self.vertices[1])
-
-        gl.glVertex3fv(self.vertices[0])
-        gl.glVertex3fv(self.vertices[4])
         
         gl.glVertex3fv(self.vertices[0])
-        gl.glVertex3fv(self.vertices[3])
+        gl.glVertex3fv(self.vertices[4])
 
         gl.glVertex3fv(self.vertices[1])
         gl.glVertex3fv(self.vertices[2])
@@ -125,65 +148,23 @@ class Section:
         
         gl.glVertex3fv(self.vertices[6])
         gl.glVertex3fv(self.vertices[7])
-
+        
         gl.glVertex3fv(self.vertices[4])
         gl.glVertex3fv(self.vertices[7])
-
-        gl.glEnd()
-
-    
-                    
+        gl.glEnd() # Find du tracé
+        gl.glPopMatrix()
+         
     # Draws the faces
     def draw(self):
         # A compléter en remplaçant pass par votre code
-
-
-      if self.parameters['edges'] == True :
-        self.drawEdges() 
-
-
+      gl.glPushMatrix()
+      self.drawEdges() 
+      gl.glTranslatef(self.parameters['position'][0], self.parameters['position'][1], self.parameters['position'][2])
       gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) # on trace les faces : GL_FILL
-
-
-      gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
-      gl.glColor3fv([0.5, 0.5, 0.5]) # Couleur gris moyen
-      gl.glVertex3fv(self.vertices[0])
-      gl.glVertex3fv(self.vertices[1])
-      gl.glVertex3fv(self.vertices[2])
-      gl.glVertex3fv(self.vertices[3])
-
-      gl.glVertex3fv(self.vertices[4])
-      gl.glVertex3fv(self.vertices[5])
-      gl.glVertex3fv(self.vertices[6])
-      gl.glVertex3fv(self.vertices[7])
-
-      gl.glVertex3fv(self.vertices[0])
-      gl.glVertex3fv(self.vertices[1])
-      gl.glVertex3fv(self.vertices[5])
-      gl.glVertex3fv(self.vertices[4])
-
-      gl.glVertex3fv(self.vertices[0])
-      gl.glVertex3fv(self.vertices[3])
-      gl.glVertex3fv(self.vertices[7])
-      gl.glVertex3fv(self.vertices[4])
-
-      gl.glVertex3fv(self.vertices[2])
-      gl.glVertex3fv(self.vertices[3])
-      gl.glVertex3fv(self.vertices[7])
-      gl.glVertex3fv(self.vertices[6])
-      
-      gl.glVertex3fv(self.vertices[1])
-      gl.glVertex3fv(self.vertices[2])
-      gl.glVertex3fv(self.vertices[6])
-      gl.glVertex3fv(self.vertices[5])
-
-      gl.glEnd()
-
-
-
-
-
-
-
-
-  
+      for i in self.faces :
+        gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
+        gl.glColor3fv(self.parameters['color'])
+        for j in i :
+          gl.glVertex3fv(self.vertices[j])
+        gl.glEnd()
+      gl.glPopMatrix()
